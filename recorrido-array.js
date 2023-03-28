b7f5aa5e5df80bc63008cb067cccd0862f00f2a1
Isaac Ramirez <risaac658@gmail.com>
2023-03-28 10:43:38 -0600

    Complemento del tema array

--- recorrido-array.js
+++ recorrido-array.js
@@ -4,6 +4,14 @@ var articulos = [
     {nombre:"Libro", costo: 4200},
     {nombre:"Celular", costo: 2000},
     {nombre:"laptop", costo: 5000},
-    {nombre: "Teclado", costo: 120},
-    {nombre"Audifonos", costo: 200},
+    {nombre:"Teclado", costo: 120},
+    {nombre:"Audifonos", costo: 200},
 ];
+//este es un anexo al array de articulos y sirve como filtro para el primero//
+var articulosfiltrados = articulos.filter(function(articulo){
+    return articulo.costo <= 500
+});
+//este es un anexo al array de articulos y sirve como filtro para el primero, pero es independiente de segundo//
+var nombrearticulo = articulos.map(function(articulo){
+    return articulo.nombre
+});
