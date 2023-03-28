ce28dd814fbdcde200ad9304f2b233437075e97c
Isaac Ramirez <risaac658@gmail.com>
2023-03-28 10:33:00 -0600

    Cambio de nombre y nuevo tema de array

---
+++ recorrido-array.js
@@ -0,0 +1,9 @@
+var articulos = [
+    {nombre: "Bici", costo: 3000},
+    {nombre:"Television", costo: 2500},
+    {nombre:"Libro", costo: 4200},
+    {nombre:"Celular", costo: 2000},
+    {nombre:"laptop", costo: 5000},
+    {nombre: "Teclado", costo: 120},
+    {nombre"Audifonos", costo: 200},
+];
